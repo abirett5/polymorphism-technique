b8157f5ef583ad1dad8aeddbfeac7789e6ec59f0
Ashfaque Hossain Abir <108783774+abirett5@users.noreply.github.com>
2024-07-01 15:13:38 +0545

    Updated

--- polymorphism.py
+++ polymorphism.py
@@ -40,9 +40,7 @@ class Pizza(Circle):
     def __init__(self, topping, radius):
         super().__init__(radius)
         self.topping = topping
-
     
-
 shapes = [Circle(4), Square(5), Triangle(6, 7), Pizza("pepperoni", 15)]
 
 for shape in shapes:
